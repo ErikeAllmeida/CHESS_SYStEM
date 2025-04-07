923763fbb7e1fc6df24f999601b0fed49a50a854
ErikeAllmeida <erikereiis@gmail.com>
2025-04-07 04:32:22 -0300

    Possible of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -8,10 +8,18 @@ public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
-		
+
 	}
+
 	@Override
 	public String toString() {
-		return "K";	}
-	
+		return "K";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+
 }
--- src/chess/pieces/Rock.java
+++ src/chess/pieces/Rock.java
@@ -8,13 +8,17 @@ public class Rock extends ChessPiece {
 
 	public Rock(Board board, Color color) {
 		super(board, color);
-		
+
 	}
 
 	@Override
 	public String toString() {
-		return "R"; 
+		return "R";
 	}
-	
-	
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
